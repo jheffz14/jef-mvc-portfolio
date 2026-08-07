ed59c0cc6004c17c3a94541eb29e5f15ac40d5e2
Jef <larrydelacruz666@gmail.com>
2026-08-07 10:03:26 +0800

    add skill and update the who am i

--- Services/SkillsService.cs
+++ Services/SkillsService.cs
@@ -12,6 +12,7 @@ namespace JefPortfolio.Services
             new Skill { Name = "JavaScript",Category = "Frontend" },
             new Skill { Name = "ASP.NET MVC",Category = "Backend"  },
             new Skill { Name = "C#", Category = "Backend"  },
+            new Skill { Name = "Python", Category = "Backend"  },
             new Skill { Name = "GitHub", Category = "Tools"    },
             new Skill { Name = "SQL",  Category = "Backend"  },
             new Skill { Name = "Bootstrap 5", Category = "Frontend" },
